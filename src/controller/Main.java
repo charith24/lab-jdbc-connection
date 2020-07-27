05cd146234ff9c9f2474085ff9a9b6e11591b383
charith24 <charith.charri@gmail.com>
2020-07-27 22:43:18 +0530

    1576033

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,16 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		
 	// Fill your code
+		ConnectionManager cm = new ConnectionManager();
+		cm.getConnection();	
 		
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,44 @@
 package utility;
 
-
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionManager {
+	public static Properties loadPropertiesFile() throws IOException {
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+				prop.load(in);
+		in.close();
+		return prop;
+	}
+	public static Connection getConnection() throws IOException {
+		String driver;
+		String url;
+		String username;
+		String password;
+		
+		
+		Properties newProp = new Properties();
+		newProp = loadPropertiesFile();
+		driver = newProp.getProperty("driver");
+		url = newProp.getProperty("url");
+		username = newProp.getProperty("username");
+		password = newProp.getProperty("password");
+		
+		try {
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url,username,password);
+			if(con!= null) {
+				System.out.println("Established");
+			}
+		}
+		catch(Exception e) {
+		System.out.println(e.getMessage());
+	}
+		return null;
 	
-	
+}
 }
